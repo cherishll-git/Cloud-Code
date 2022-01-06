8388605cb4f197a6341e5678b0a1e9b9448b563d
CherishLL <2860025067@qq.com>
2022-01-06 15:00:25 +0800

    Eureka注册中心

--- SpringCloudEureka/order_service/src/main/java/com/lyq/OrderApplication.java
+++ SpringCloudEureka/order_service/src/main/java/com/lyq/OrderApplication.java
@@ -3,6 +3,9 @@ package com.lyq;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.lyq.mapper")
 @SpringBootApplication
@@ -10,6 +13,17 @@ public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    /**
+     * 注册RestTemplate实例
+     * @LoadBalanced 注解作用是开启负载均衡
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
--- SpringCloudEureka/order_service/src/main/java/com/lyq/service/OrderService.java
+++ SpringCloudEureka/order_service/src/main/java/com/lyq/service/OrderService.java
@@ -2,8 +2,10 @@ package com.lyq.service;
 
 import com.lyq.mapper.OrderMapper;
 import com.lyq.pojo.Order;
+import com.lyq.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -11,13 +13,17 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // TODO 2.查询用户
-
+        // 2.查询用户,利用restTemplate发送http请求，查询用户
+        String url = "http://userservice/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
         // 3.封装user信息
-        order.setUser(null);
+        order.setUser(user);
         // 4.返回
         return order;
     }
